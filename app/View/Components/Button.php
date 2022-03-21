d596fe32ccb20e0ee1604164599603ff7e03dcd1
milovdpas <mpmpas1@avans.nl>
2022-03-21 20:28:51 +0100

    first version of component

---
+++ app/View/Components/Button.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Button extends Component
+{
+    /**
+     * The alert type.
+     *
+     * @var string
+     */
+    public string $type;
+
+    /**
+     * The alert message.
+     *
+     * @var string|null
+     */
+    public string|null $link;
+
+    /**
+     * Create a new component instance.
+     * @param string $type
+     * @param string|null $link
+     */
+    public function __construct(string $type, string|null $link = null)
+    {
+        $this->type = $type;
+        $this->link = $link;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return Application|Factory|View
+     */
+    public function render(): View|Factory|Application
+    {
+        return view('components.button');
+    }
+}
---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,33 @@
+@if($link)
+    <a href="{{$link}}"
+    @switch($type)
+        @case("primary")
+            {{ $attributes->merge(['class' => 'btn btn-primary']) }}
+            @break
+            @case("secondary")
+            {{ $attributes->merge(['class' => 'btn btn-secondary']) }}
+            @break
+            @case("remove")
+            {{ $attributes->merge(['class' => 'btn btn-danger']) }}
+            @break
+        @endswitch
+    >
+        {{$slot}}
+    </a>
+@else
+    <button type="submit"
+    @switch($type)
+        @case("primary")
+            {{ $attributes->merge(['class' => 'btn btn-primary']) }}
+            @break
+            @case("secondary")
+            {{ $attributes->merge(['class' => 'btn btn-secondary']) }}
+            @break
+            @case("remove")
+            {{ $attributes->merge(['class' => 'btn btn-danger']) }}
+            @break
+        @endswitch
+    >
+        {{$slot}}
+    </button>
+@endif
