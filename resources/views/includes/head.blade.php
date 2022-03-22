29b3feee26f87ae3f7820478cb7dcc4a407def2c
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2022-03-22 16:48:10 +0100

    first try

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="Saquib" content="Blade">
+<title>Accessibility questionnaire dashboard</title>
+
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
