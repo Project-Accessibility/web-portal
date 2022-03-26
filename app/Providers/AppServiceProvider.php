c7b1f65872d666fd983a7630105c5fccf9af8caf
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2022-03-26 16:11:20 +0100

    fix for including css and js

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\View\Components\Button;
 use App\View\Components\Input;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,11 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot()
   {
+    if(config('APP_ENV') !== 'local') {
+        URL::forceScheme('https');
+    }
+
+
     Blade::component('button', Button::class);
     Blade::component('input', Input::class);
   }
