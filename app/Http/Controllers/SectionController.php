88c9fcfd6ffe1430db38089f7bafdf0bf1abb7cc
milovdpas <mpmpas1@avans.nl>
2022-04-06 01:28:35 +0200

    v1 of section CRUD

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -9,6 +9,7 @@ use App\Models\Section;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
 
 class SectionController extends Controller
