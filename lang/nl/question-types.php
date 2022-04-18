e461cb95fc7290016008f044f2777287b689f1d9
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2022-04-18 02:38:15 +0200

    removed comment

--- lang/nl/question-types.php
+++ lang/nl/question-types.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Authentication Language Lines
-|--------------------------------------------------------------------------
-|
-| The following language lines are used during authentication for various
-| messages that we need to display to the user. You are free to modify
-| these language lines according to your application's requirements.
-|
-*/
-
 return [
     'placeholder' => 'Placeholder:',
     'multiple' => 'Meerdere antwoorden mogelijk?',
