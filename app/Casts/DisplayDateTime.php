dc1396be4bc9ceb1055ae58f2648955e9abb7424
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2022-04-26 21:09:59 +0200

    V1.0

--- app/Casts/DisplayDateTime.php
+++ app/Casts/DisplayDateTime.php
@@ -2,18 +2,7 @@
 
 namespace App\Casts;
 
-use Carbon\Carbon;
-use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
-
-class DisplayDateTime implements CastsAttributes
+class DisplayDateTime
 {
-    public function get($model, string $key, $value, array $attributes)
-    {
-        return Carbon::parse($value)->translatedFormat('l d F Y \o\m h:i');
-    }
 
-    public function set($model, string $key, $value, array $attributes)
-    {
-        return Carbon::createFromFormat('l d F Y \o\m h:i', $value);
-    }
 }
