03251975b7ad3cdf803db6a54ad1fb0ad69b9b40
milovdpas <mpmpas1@avans.nl>
2022-04-28 16:53:45 +0200

    preview for photo and video

--- resources/views/admin/question/answer.blade.php
+++ resources/views/admin/question/answer.blade.php
@@ -9,40 +9,84 @@
                 <h3 class="h4">{{ $answers[0]->option()->type->display() }}</h3>
                 @switch($answers[0]->option()->type)
                     @case(\App\Enums\QuestionOptionType::OPEN)
-                        <p>{{$answers[0]->answer['text']}}</p>
-                        @break;
+                    <p>{{$answers[0]->answer['text']}}</p>
+                    @break;
                     @case(\App\Enums\QuestionOptionType::VOICE)
-                        <div class="row gy-2">
-                            @foreach($answers as $answer)
-                                <audio class="col-md-6" controls>
-                                    <source src="{{ $answer->answer['link'] }}" type="audio/mpeg">
-                                </audio>
-                            @endforeach
-                        </div>
-                        @break;
+                    <div class="row gy-2">
+                        @foreach($answers as $answer)
+                            <audio class="col-md-6" controls>
+                                <source src="{{ $answer->answer['link'] }}" type="audio/mpeg">
+                            </audio>
+                        @endforeach
+                    </div>
+                    @break;
                     @case(\App\Enums\QuestionOptionType::IMAGE)
-                        <div class="row gy-2">
-                            @foreach($answers as $answer)
-                                <img src="{{ $answer->answer['link'] }}" class="col-md-6" alt="image">
-                            @endforeach
-                        </div>
-                        @break;
+                    <div class="row gy-2">
+                        @foreach($answers as $answer)
+                            <a href="#" class="col-md-6" data-bs-toggle="modal"
+                               data-bs-target="#previewImage{{$answer->id}}">
+                                <img src="{{ $answer->answer['link'] }}" class="w-100" alt="image">
+                            </a>
+                            <div class="modal fade" id="previewImage{{$answer->id}}" tabindex="-1"
+                                 aria-labelledby="previewImageLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered modal-xl">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="previewImageLabel">Afbeelding preview</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <img src="{{ $answer->answer['link'] }}" class="w-100" alt="image">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                    @break;
                     @case(\App\Enums\QuestionOptionType::VIDEO)
-                        <div class="row gy-2">
-                            @foreach($answers as $answer)
-                                <video src="{{ $answer->answer['link'] }}" class="col-md-6" controls>
-                            @endforeach
-                        </div>
+                    <div class="row gy-2">
+                        @foreach($answers as $answer)
+                            <a href="#" class="col-md-6" data-bs-toggle="modal"
+                               data-bs-target="#previewVideo{{$answer->id}}">
+                                <video src="{{ $answer->answer['link'] }}" class="w-100"></video>
+                                <div class="modal fade" id="previewVideo{{$answer->id}}" tabindex="-1"
+                                     aria-labelledby="previewVideoLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered modal-xl">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="previewVideoLabel">Video preview</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <video id="video{{$answer->id}}" src="{{ $answer->answer['link'] }}"
+                                                       class="w-100" controls></video>
+                                            </div>
+                                            <script>
+                                                const modal = document.getElementById('previewVideo' + '{{$answer->id}}');
+                                                const video = document.getElementById('video' + '{{$answer->id}}');
+                                                modal.addEventListener('hide.bs.modal', function () {
+                                                    video.pause();
+                                                });
+                                            </script>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        @endforeach
+                    </div>
                     @break;
                     @case(\App\Enums\QuestionOptionType::MULTIPLE_CHOICE)
-                        <div class="row gy-2">
-                            <ul class="list-group">
-                                @foreach($answers[0]->answer['options'] as $answer)
-                                    <li class="list-group-item {{in_array($answer, $answers[0]->answer['selected']) ? 'selected' : ''}}">{{ $answer }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        @break;
+                    <div class="row gy-2">
+                        <ul class="list-group">
+                            @foreach($answers[0]->answer['options'] as $answer)
+                                <li class="list-group-item {{in_array($answer, $answers[0]->answer['selected']) ? 'selected' : ''}}">{{ $answer }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @break;
                 @endswitch
             </div>
         @endforeach
