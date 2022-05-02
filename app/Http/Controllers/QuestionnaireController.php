8a86b23ab31156c0c7e8abf01b8eee40fe0a76d4
milovdpas <mpmpas1@avans.nl>
2022-05-02 14:10:48 +0200

    update Seeder

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -237,7 +237,7 @@ class QuestionnaireController extends Controller
                 'participantHeaders',
                 'participantKeys',
                 'participantRowLink',
-                'questionSections'
+                'questionSections',
             ),
         );
     }
