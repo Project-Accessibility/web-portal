e1efd317a511874c5e1e7804e32f1ba2868636d0
milovdpas <mpmpas1@avans.nl>
2022-05-02 16:59:30 +0200

    make performance better

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -103,17 +103,6 @@ class Question extends Model
                 }
             }
         }
-//        foreach ($options as $option) {
-//            $tempAnswers = $option->answers()->get();
-//            foreach ($tempAnswers as $temp) {
-//                $answerExists = $answers
-//                    ->where('participant_id', '=', $temp->participant_id)
-//                    ->first();
-//                if ($answerExists == null) {
-//                    $answers->push($temp);
-//                }
-//            }
-//        }
         return $answers;
     }
 }
