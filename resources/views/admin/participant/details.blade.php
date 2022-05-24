d1c4ae4f2eb906cefda6f0e166dd7638350bd552
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2022-05-24 20:53:13 +0200

    linking also works

--- resources/views/admin/participant/details.blade.php
+++ resources/views/admin/participant/details.blade.php
@@ -23,18 +23,25 @@
                 <div class="d-flex justify-content-center h-50">
                     <div class="vr"></div>
                 </div>
-                <span class="badge badge-pill bg-light border rounded-circle">&nbsp;</span>
+                <span aria-hidden="true" class="badge badge-pill bg-light border rounded-circle">&nbsp;</span>
                 <div class="d-flex justify-content-center h-50">
                     <div class="vr"></div>
                 </div>
             </div>
             <div class="col py-2">
-                <div class="card">
-                    <div class="card-body d-flex flex-column gap-2">
-                        <b class="card-text">{{ 'Vraag \'' . $question->title . '\' van onderdeel \'' . $question->section->title . '\' beantwoord.' }}</b>
-                        <small>{{ $question->latest_answer }}</small>
+                <a class="text-decoration-none" href="{{ route('questions.answer', [$research->id,$questionnaire->id, $question->section->id, $question->id, $participant->id]) }}">
+                    <div class="card participant-timeline-card">
+                        <div class="card-body d-flex justify-content-between align-items-center gap-2">
+                            <div class="d-flex flex-column">
+                                <b class="card-text">{{ 'Vraag \'' . $question->title . '\' van onderdeel \'' . $question->section->title . '\' beantwoord.' }}</b>
+                                <small>{{ $question->latest_answer }}</small>
+                            </div>
+                            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                            </svg>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
     @endforeach
