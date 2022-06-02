369ea6f4e9d7b76556602f334ca5b9d9018e2ee1
milovdpas <mpmpas1@avans.nl>
2022-06-02 21:40:17 +0200

    change full size popup of video's and images

--- resources/views/admin/question/answer.blade.php
+++ resources/views/admin/question/answer.blade.php
@@ -32,72 +32,116 @@
                     @break;
                     @case(\App\Enums\QuestionOptionType::IMAGE)
                     <div class="row gy-2">
-                        @foreach($answer->values as $link)
-                            <a href="#" class="col-lg-4 col-md-6" data-bs-toggle="modal"
-                               data-bs-target="#previewImage{{$loop->index}}">
-                                <img src="{{ $link }}" class="fit-image-cover" alt="image">
-                            </a>
-                            <div class="modal fade" id="previewImage{{$loop->index}}" tabindex="-1"
-                                 aria-labelledby="previewImageLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered modal-xl">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="previewImageLabel">Volledige grootte</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                    aria-label="Close"></button>
+                        <section id="gallery">
+                            <section id="images">
+                                <div class="row">
+                                    @foreach($answer->values as $link)
+                                        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                                            <a href="#" data-bs-toggle="modal"
+                                               data-bs-target="#previewImage{{$loop->index}}">
+                                                <div
+                                                    class="bg-image hover-overlay ripple shadow-1-strong rounded"
+                                                    data-ripple-color="light"
+                                                >
+                                                    <img src="{{ $link }}"
+                                                         class="fit-cover" alt="image{{$loop->index}}"/>
+                                                </div>
+                                            </a>
                                         </div>
-                                        <div class="modal-body">
-                                            <img src="{{ $link }}" class="w-100" alt="image">
+                                    @endforeach
+                                </div>
+                            </section>
+                            <section id="modals">
+                                @foreach($answer->values as $link)
+                                    <div
+                                        class="modal fade"
+                                        id="previewImage{{$loop->index}}"
+                                        tabindex="-1"
+                                        aria-labelledby="image {{$loop->index}} modal"
+                                        aria-hidden="true"
+                                    >
+                                        <div class="modal-dialog modal-dialog-centered p-0">
+                                            <div class="modal-content p-3">
+                                                <img class="fit-contain" src="{{ $link }}" alt="image{{$loop->index}}"/>
+
+                                                <div class="text-center pt-3">
+                                                    <button type="button" class="btn btn-secondary"
+                                                            data-bs-dismiss="modal">
+                                                        Sluiten
+                                                    </button>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                        @endforeach
+                                @endforeach
+                            </section>
+                        </section>
                     </div>
                     @break;
                     @case(\App\Enums\QuestionOptionType::VIDEO)
-                    <div class="row gy-2">
-                        @foreach($answer->values as $link)
-                            <a href="#" class="col-lg-4 col-md-6 d-flex justify-content-center align-items-center"
-                               data-bs-toggle="modal"
-                               data-bs-target="#previewVideo{{$loop->index}}">
-                                <video src="{{ $link }}" class="w-100"></video>
-                                <i class="bi bi-play position-absolute text-white h1"></i>
-                                <div class="modal fade" id="previewVideo{{$loop->index}}" tabindex="-1"
-                                     aria-labelledby="previewVideoLabel" aria-hidden="true">
-                                    <div class="modal-dialog modal-dialog-centered modal-xl">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="previewVideoLabel">Volledige grootte</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                        aria-label="Close"></button>
+                    <section id="gallery">
+                        <section id="images">
+                            <div class="row">
+                                @foreach($answer->values as $link)
+                                    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                                        <a href="#" data-bs-toggle="modal"
+                                           data-bs-target="#previewVideo{{$loop->index}}">
+                                            <div
+                                                class="bg-image hover-overlay ripple shadow-1-strong rounded"
+                                                data-ripple-color="light"
+                                            >
+                                                <video id="video{{$loop->index}}" src="{{ $link }}"
+                                                       class="fit-cover"></video>
                                             </div>
-                                            <div class="modal-body">
-                                                <video id="video{{$answer->id}}" src="{{ $link }}"
-                                                       class="w-100 h-100" controls></video>
+                                        </a>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </section>
+                        <section id="modals">
+                            @foreach($answer->values as $link)
+                                <div
+                                    class="modal fade"
+                                    id="previewVideo{{$loop->index}}"
+                                    tabindex="-1"
+                                    aria-labelledby="video {{$loop->index}} modal"
+                                    aria-hidden="true"
+                                >
+                                    <div class="modal-dialog modal-lg modal-dialog-centered">
+                                        <div class="modal-content p-3">
+                                            <video class="fit-contain" id="video{{$loop->index}}" src="{{ $link }}" controls></video>
+
+                                            <div class="text-center pt-3">
+                                                <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">
+                                                    Sluiten
+                                                </button>
                                             </div>
-                                            <script>
-                                                const modal = document.getElementById('previewVideo' + '{{$answer->id}}');
-                                                const video = document.getElementById('video' + '{{$answer->id}}');
-                                                modal.addEventListener('hide.bs.modal', function () {
-                                                    video.pause();
-                                                });
-                                            </script>
                                         </div>
                                     </div>
                                 </div>
-                            </a>
-                        @endforeach
-                    </div>
+                                <script>
+                                    const modal = document.getElementById('previewVideo{{$loop->index}}');
+                                    const video = document.getElementById('video{{$loop->index}}');
+                                    modal.addEventListener('hide.bs.modal', function () {
+                                        video.pause();
+                                    });
+                                </script>
+                            @endforeach
+                        </section>
+                    </section>
                     @break;
                     @case(\App\Enums\QuestionOptionType::MULTIPLE_CHOICE)
                     <ul class="list-group">
                         @foreach($answer->option->extra_data['values'] as $option)
                             <li class="list-group-item {{in_array($option, $answer->values) ? 'selected' : ''}}" {{in_array($option, $answer->values) ? 'aria-selected=true' : 'aria-selected=false'}}>
                                 @if(in_array($option, $answer->values))
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16" stroke-width="1" stroke="white">
-                                        <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
-                                        <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                         viewBox="0 0 16 16" stroke-width="1" stroke="white">
+                                        <path
+                                            d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+                                        <path
+                                            d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
                                     </svg>
                                 @endif
                                 {{ $option }}</li>
@@ -105,7 +149,8 @@
                     </ul>
                     @break;
                     @case(\App\Enums\QuestionOptionType::RANGE)
-                    <x-input type="range" name="range" :extraData="$answer->option->extra_data->toArray()" :value="$answer->values[0]" :disabled="true"></x-input>
+                    <x-input type="range" name="range" :extraData="$answer->option->extra_data->toArray()"
+                             :value="$answer->values[0]" :disabled="true"></x-input>
                     @break;
                 @endswitch
             </div>
