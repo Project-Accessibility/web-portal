14b6c8c491e47c97a5e760da47be4140f842eba9
milovdpas <mpmpas1@avans.nl>
2022-06-02 23:30:31 +0200

    add password reset link

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,16 @@
 <?php
 
-use App\Http\Controllers\Auth\ForgotPasswordController;
-use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\ParticipantController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\QuestionnaireController;
 use App\Http\Controllers\ResearchController;
 use App\Http\Controllers\ResultController;
-use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\SectionController;
 use App\Http\Requests\TestInputsRequest;
-use App\Models\Participant;
 use App\Models\Questionnaire;
 use App\Models\Research;
 use Illuminate\Support\Facades\App;
-use App\Http\Controllers\ParticipantController;
 use Illuminate\Support\Facades\Route;
-
-Route::middleware('guest')
-    ->prefix('/password')
-    ->group(function () {
-        Route::controller(ForgotPasswordController::class)->group(function () {
-            Route::get('/reset', 'showLinkRequestForm')->name(
-                'password.request',
-            );
-            Route::post('/email', 'sendResetLinkEmail')->name('password.email');
-        });
-        Route::controller(ResetPasswordController::class)->group(function () {
-            Route::get('/reset-password/{token}', 'showResetForm')->name(
-                'password.reset',
-            );
-            Route::post('/reset', 'reset')->name('password.update');
-        });
-    });
 
 Route::middleware('auth')->group(function () {
     Route::redirect('/', '/researches')->name('home');
