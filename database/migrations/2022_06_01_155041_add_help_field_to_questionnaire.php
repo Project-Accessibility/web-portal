29689de3d62e43363b722ef0167da8354d074189
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2022-06-06 10:57:34 +0200

    Change requests from Milo

--- database/migrations/2022_06_01_155041_add_help_field_to_questionnaire.php
+++ database/migrations/2022_06_01_155041_add_help_field_to_questionnaire.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('questionnaires', function (Blueprint $table) {
-            $table->string('help')->after('instructions')->nullable();
+            $table->text('help')->after('instructions')->nullable();
         });
     }
 
