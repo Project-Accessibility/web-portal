bd42747a2d42412bc04cf9c44f48615f120d4674
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2022-06-14 00:14:03 +0200

    Database now works

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,10 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, DatabaseTransactions, DatabaseMigrations;
+    use CreatesApplication, RefreshDatabase;
 }
