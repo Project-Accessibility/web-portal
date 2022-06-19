e1abbff95acc69cb1a4dad13ced14016c634ea6a
milovdpas <mpmpas1@avans.nl>
2022-06-19 23:27:29 +0200

    init project devops

--- resources/views/errors/500.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('errors.minimal')
-
-@section('title', 'Server error')
-@section('code', '500')
-@section('message')
-    <div class="h2">
-        {{$message ?? 'Er is een onbekende fout opgetreden.'}}
-    </div>
-@endsection
