d6da0b811ea5e54c592a7e1f297cc362ad0aa539
MartijnAmbagtsheer <63644928+MartijnAmbagtsheer@users.noreply.github.com>
2022-11-05 15:01:51 +0100

    prettier

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,9 +35,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (
-            config('app.env') !== 'local'
-        ) {
+        if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         } else {
             URL::forceScheme('http');
